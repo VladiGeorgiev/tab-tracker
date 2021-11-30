f1ee211f40090115e00e758d4648891d4855dd45
VladiGeorgiev <georgiev.vlad@gmail.com>
2021-11-30 16:58:08 +0200

    set up of the express js server

---
+++ server/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 13
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+};
